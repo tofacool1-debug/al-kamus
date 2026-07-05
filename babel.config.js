9300b64bc7e543490e26dc29814b53f512746f8f
tofacool1-debug <tofacool1@gmail.com>
2026-07-06 06:52:35 +0700

    Update Babel configuration to simplify presets
    Removed unstable_transformImportMeta option and added module-resolver plugin with alias.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          '@': './',
+        }
+      }]
+    ]
   };
 };
