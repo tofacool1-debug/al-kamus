47cb0818d06a26a2c42b544a98251423afeb5128
tofacool1-debug <tofacool1@gmail.com>
2026-07-07 04:50:46 +0000

    edit masalah build

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,13 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ['module-resolver', {
-        root: ['./'],
-        alias: {
-          '@': './',
-        }
-      }]
-    ]
-  };
+const config = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./'],
+      alias: {
+        '@': './',
+      }
+    }]
+  ]
 };
+
+export default config;
